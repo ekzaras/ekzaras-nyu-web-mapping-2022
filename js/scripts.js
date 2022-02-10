e68626a7ee36389d1d0905e9e1f9f3f24738c6c1
ekzaras <ekz209@nyu.edu>
2022-02-10 11:03:52 -0500

    update list and caption style

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 $('#Al-Andaluse-Garden').on('click', function() {
-  $(".right-half").html("<img src='https://www.egypttoday.com/siteimages/Larg/71578.jpg'/> Located on the island of Zamalek");
+  $(".right-half").html("<img src='https://www.egypttoday.com/siteimages/Larg/71578.jpg'/> <p> <div class='right-half-caption'> Located on the island of Zamalek, this garden was designed to resemble the gardens of the Alhambra of Granada. It is currently closed to the public, supposedly for renovations, but some speculate that it will never reopen. </div></p>");
 })
 
 $('#Al-Horreya-Garden').on('click', function() {
@@ -8,11 +8,11 @@ $('#Al-Horreya-Garden').on('click', function() {
 })
 
 $('#Aquarium-Grotto-Garden').on('click', function() {
-  $(".right-half").html("<img src='https://cairoscene.com/Content/Admin/Uploads/Articles/ArticlesMainPhoto/1131069/a03d8898-6776-4a65-bc46-64e0ebe371f0.jpg'/> Located on the island of Zamalek");
+  $(".right-half").html("<img src='https://cairoscene.com/Content/Admin/Uploads/Articles/ArticlesMainPhoto/1131069/a03d8898-6776-4a65-bc46-64e0ebe371f0.jpg'/> <p> Located on the island of Zamalek </p>");
 })
 
 $('#Al-Azhar-Park').on('click', function() {
-  $(".right-half").html("<img src='https://www.likealocalguide.com/media/cache/34/44/3444e7688732e87e9f3fc357a23b6264.jpg'/> Al Azhar Park is located on the eastern edge of Cairo and covers 30-hectares of land. <img src='https://d1zah1nkiby91r.cloudfront.net/s3fs-public/styles/featured_content/public/aktc-egypt-cairopark_after.jpg?c=5aaadb4c5811361d1484343ca7f7ebe9'/>");
+  $(".right-half").html("<img src='https://www.likealocalguide.com/media/cache/34/44/3444e7688732e87e9f3fc357a23b6264.jpg'/> <p> <div class='right-half-caption'> Al Azhar Park is located on the eastern edge of Cairo and covers 30-hectares of land. </div></p> <img src='https://d1zah1nkiby91r.cloudfront.net/s3fs-public/styles/featured_content/public/aktc-egypt-cairopark_after.jpg?c=5aaadb4c5811361d1484343ca7f7ebe9'/>");
 });
 
 $('#Manial-Garden').on('click', function() {
@@ -20,5 +20,5 @@ $('#Manial-Garden').on('click', function() {
 })
 
 $('#Orman-Botanical-Garden').on('click', function() {
-	$(".right-half").html("<img src='https://egyptopia.com/shared/images/Geographic/33/big/-el-orman-gardens-_33167_33167.jpg'/> Located in Giza to the west of the Nile River, the Orman Botanical Garden was established in 1875 by Khedive Isma'il Pasha.")
+	$(".right-half").html("<img src='https://egyptopia.com/shared/images/Geographic/33/big/-el-orman-gardens-_33167_33167.jpg'/> <p> <div class='right-half-caption'> Located in Giza to the west of the Nile River, the Orman Botanical Garden was established in 1875 by Khedive Isma'il Pasha. </div></p>")
 })
